97dc596eb35ce458166fdeb2b6ab2dd6f2018c36
oliver12313 <87396337+oliver12313@users.noreply.github.com>
2021-07-20 17:59:46 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,55 +1,58 @@
-function makeEnemy() {
-    let evilBurger  = sprites.create(img`
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . e e e e e e e e e . .
-        . . . . e e . 2 . . 2 . . e e .
-        . . e e . . . . . . . . . . . e
-        . . e 7 7 7 7 7 7 7 7 7 7 7 7 e
-        . . e 2 2 2 2 2 2 2 2 2 2 2 2 e
-        . . e e 5 5 5 5 5 5 5 2 2 2 2 e
-        . . . e 5 5 . 5 5 5 5 5 5 5 5 e
-        . . . e c c c c c c c c 5 5 5 e
-        . . . e e . c c c c c c c c c e
-        . . . . e e e e e e e e e e e e
-        . . . . . . . . . . e e e e e .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-     `,SpriteKind.Enemy)
-    evilBurger.setVelocity(40, 60)   
-    evilBurger.setPosition(randint(0,160), 0)  
-    evilBurger.setBounceOnWall(true)
-}
-
-controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     fish.setImage(fishImageLeft)
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function() {
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     fish.setImage(fishImageRight)
 })
-let fishImageRight=img`
-    . . . . . . . . . . 1 . . . . .
-    . . . . . . . . . 1 1 1 . . . .
-    . . . . . . . . 1 1 . 1 . . . .
-    . . . . . . . . 1 . . 1 . . . .
-    . . . . . . 1 1 1 1 1 1 1 1 1 1
-    . . . . . 1 1 6 6 6 6 6 6 6 6 1
-    . . . . 1 1 6 6 6 6 6 6 6 6 2 1
-    . . . 1 6 6 6 6 6 6 6 6 6 6 6 1
-    . . 1 6 6 6 6 6 6 6 6 6 6 6 6 1
-    . . . 1 1 6 6 6 6 6 6 6 6 6 6 1
-    . . . . 1 6 6 6 6 6 6 6 6 6 6 1
-    . . . . 1 6 6 6 6 6 6 6 6 6 2 1
-    . . . . 1 1 6 6 6 6 6 6 6 6 1 1
-    . . . . . 1 1 1 1 1 1 1 1 1 . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`
-let fishImageLeft = fishImageRight.clone()
+function makeEnemy () {
+    evilBurger = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . e e e e e e e e e . . 
+        . . . . e e . 2 . . 2 . . e e . 
+        . . e e . . . . . . . . . . . e 
+        . . e 7 7 7 7 7 7 7 7 7 7 7 7 e 
+        . . e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        . . e e 5 5 5 5 5 5 5 2 2 2 2 e 
+        . . . e 5 5 . 5 5 5 5 5 5 5 5 e 
+        . . . e c c c c c c c c 5 5 5 e 
+        . . . e e . c c c c c c c c c e 
+        . . . . e e e e e e e e e e e e 
+        . . . . . . . . . . e e e e e . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    evilBurger.setVelocity(40, 60)
+    evilBurger.setPosition(randint(0, 160), 0)
+    evilBurger.setBounceOnWall(true)
+}
+let evilBurger: Sprite = null
+let fish: Sprite = null
+let fishImageLeft: Image = null
+let fishImageRight: Image = null
+fishImageRight = img`
+    . . . . . . . . . . 1 . . . . . 
+    . . . . . . . . . 1 1 1 . . . . 
+    . . . . . . . . 1 1 . 1 . . . . 
+    . . . . . . . . 1 . . 1 . . . . 
+    . . . . . . 1 1 1 1 1 1 1 1 1 1 
+    . . . . . 1 1 6 6 6 6 6 6 6 6 1 
+    . . . . 1 1 6 6 6 6 6 6 6 6 2 1 
+    . . . 1 6 6 6 6 6 6 6 6 6 6 6 1 
+    . . 1 6 6 6 6 6 6 6 6 6 6 6 6 1 
+    . . . 1 1 6 6 6 6 6 6 6 6 6 6 1 
+    . . . . 1 6 6 6 6 6 6 6 6 6 6 1 
+    . . . . 1 6 6 6 6 6 6 6 6 6 2 1 
+    . . . . 1 1 6 6 6 6 6 6 6 6 1 1 
+    . . . . . 1 1 1 1 1 1 1 1 1 . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `
+fishImageLeft = fishImageRight.clone()
 fishImageLeft.flipX()
-let fish = sprites.create(fishImageLeft,SpriteKind.Player)
+fish = sprites.create(fishImageLeft, SpriteKind.Player)
 fish.setPosition(80, 110)
-controller.moveSprite(fish,100, 0)
+controller.moveSprite(fish, 100, 0)
 fish.setStayInScreen(true)
 makeEnemy()
